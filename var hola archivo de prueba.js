a189e47bdbc620fc0765e138dfb2f37e7323c45c
ThisJoseph <montanajosemiguel78@gmail.com>
2022-11-06 13:35:18 -0500

    segunda function

--- var hola archivo de prueba.js
+++ var hola archivo de prueba.js
@@ -3,3 +3,9 @@ var hola
 function saludo(nombre, edad) {
 	console.log('hola, mi nombre es ' + nombre + ' tengo '+ edad + 'años.')
 }
+
+
+
+function comida(comida, comida2) {
+	console.log('a mi me gusta la ' + comodia + ', y ami amigo le gusta el' + comida2)
+}
